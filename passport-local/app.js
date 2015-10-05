1de98aa2c3eeaaa958f6351908294efe931c335c
Ubuntu <ubuntu@ip-172-31-25-188.us-west-1.compute.internal>
2015-10-05 22:17:38 +0000

    secret

--- passport-local/app.js
+++ passport-local/app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('express-session')({
-    secret: 'epicmath',
+    secret: 'mySecret',
     resave: false,
     saveUninitialized: false
 }));
