6b190366098be625c64b280021718c3b22830e60
Ubuntu <ubuntu@ip-172-31-25-188.us-west-1.compute.internal>
2015-10-05 15:54:37 +0000

    unauthorized user

--- passport-local/routes/index.js
+++ passport-local/routes/index.js
@@ -15,7 +15,7 @@ router.get('/register', function(req, res) {
 router.post('/register', function(req, res) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
-            return res.render('register', { account : account });
+            return res.render("register", {info: "Sorry. That username already exists. Try again."});
         }
 
         passport.authenticate('local')(req, res, function () {
