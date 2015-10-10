9d6e62acbe1e73d420aa40ad8c5d92a7a802967a
Ubuntu <ubuntu@ip-172-31-25-188.us-west-1.compute.internal>
2015-10-10 05:14:19 +0000

    addition

---
+++ passport-social/fb.js
@@ -0,0 +1,6 @@
+// facebook app settings
+module.exports = {
+	'appID' : '<id>',
+	'appSecret' : '<secret>',
+	'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
+}
---
+++ passport-social/twitter.js
@@ -0,0 +1,6 @@
+// twitter app settings
+module.exports = {
+	'apikey' : '<your_api_key>',
+	'apisecret' : '<your_api_secret>',
+	'callbackUrl' : 'http://127.0.0.1:3000/login/twitter/callback'
+}
